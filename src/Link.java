c56639791ccae168d4dec06c7b84f729acedf3f6
Simon Roehrl <roehrlsi45852@ohm-hochschule.de>
2013-11-21 18:58:41 +0100

    fixed bug: maxspeed is 0 by default, added some methods

--- src/Link.java
+++ src/Link.java
@@ -2,12 +2,12 @@
 import java.util.Map;
 
 public class Link {
+
     private Crossing from;
     private Crossing to;
-    
     private long from_id;
     private long to_id;
-    
+
     // TODO length und speed muss nicht unbedingt gespeichert werden, time reicht aus
     /**
      * in meters
@@ -18,22 +18,39 @@ public class Link {
      * in seconds
      */
     private double time;
-        
+
     public Link(long from_id, long to_id, int length, int lsiclass, int maxspeed) {
         this.from_id = from_id;
         this.to_id = to_id;
         this.length = length;
         int lsiSpeed = LSISpeed.getSpeedById(lsiclass);
-        this.speed = maxspeed < lsiSpeed ? maxspeed : lsiSpeed;
-        this.time = (double) this.length / (this.speed * 1000 / 60 / 60 );
-        
-        
+
+        if (maxspeed != 0 && maxspeed < lsiSpeed) {
+            this.speed = maxspeed;
+        } else {
+            this.speed = lsiSpeed;
+        }
+
+        this.time = (double) this.length / (double) (this.speed * 1000 / 60 / 60);
     }
-    
-    public void setCrossingReferences(Map<Long,Crossing> crossings) {
+
+    public void setCrossingReferences(Map<Long, Crossing> crossings) {
         this.from = crossings.get(new Long(from_id));
         this.to = crossings.get(new Long(to_id));
         this.from.outgoingLinks.add(this);
     }
-             
+
+    /**
+     * @return the time
+     */
+    public double getTime() {
+        return time;
+    }
+
+    /**
+     * @return the to
+     */
+    public Crossing getTargetCrossing() {
+        return to;
+    }
 }
